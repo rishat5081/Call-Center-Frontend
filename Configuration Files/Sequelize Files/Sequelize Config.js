b9451c44df5cabf50bd0205a40a683592552c4e9
Saad Sohail <rishat.5081@gmail.com>
2023-07-05 19:24:48 +0500

    update credentials

--- Configuration Files/Sequelize Files/Sequelize Config.js
+++ Configuration Files/Sequelize Files/Sequelize Config.js
@@ -1,25 +1,25 @@
-const { Sequelize } = require('sequelize')
-require('dotenv').config()
-const sequelize = new Sequelize(process.env.database, process.env.user, '', {
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const sequelize = new Sequelize(
+  process.env.database,
+  process.env.user,
+  process.env.password,
+  {
     host: process.env.host,
-    dialect: 'mysql'
-});
+    dialect: "mysql",
+  }
+);
 try {
-    sequelize.authenticate().then((res) => {
-        console.log('Connection has been established successfully.')
+  sequelize
+    .authenticate()
+    .then((res) => {
+      console.log("Connection has been established successfully.");
     })
-        .catch((error) => {
-            console.log(error)
-        })
-
+    .catch((error) => {
+      console.log(error);
+    });
 } catch (error) {
-    console.error('Unable to connect to the database:');
+  console.error("Unable to connect to the database:");
 }
 
-
-
-
-
-
-
-module.exports = sequelize
+module.exports = sequelize;
